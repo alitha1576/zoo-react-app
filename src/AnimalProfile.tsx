76ee6140ba15940cf2c9a8a993c0e9d3ff710665
Anna Petrova <ann.undine@gmail.com>
2025-03-25 11:12:27 +0100

    Add filter by animal type (all, birds, mammals)

--- src/AnimalProfile.tsx
+++ src/AnimalProfile.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { animalsData } from "./animals";
 
+export default function AnimalProfile () {
+     const [filter, setFilter] = useState('all');
 
-const AnimalsItems = animalsData.map((animal) => (
+const filteredAnimals = filter === 'all' ? animalsData : animalsData.filter(animal => animal.type === filter);
+
+const AnimalsItems = filteredAnimals.map((animal) => (
     <div key={animal.name} className="animalCard">
         <div className="animalImg" style={{ backgroundImage: `url(${animal.img})` }}></div>
         <div className="animalInfo">
@@ -13,9 +18,14 @@ const AnimalsItems = animalsData.map((animal) => (
     </div>
 ));
 
-
-export default function AnimalProfile () {
     return (
+        <>
+<div className="buttonsSection">
+        <button onClick={() => setFilter('all')} id='allAnimals'>All animals</button>
+        <button onClick={() => setFilter('Mammal')} id='mammals'>Mammals</button>
+        <button onClick={() => setFilter('Bird')} id='birds'>Birds</button>
+</div>
      <div className="animalContainer">{AnimalsItems}</div>
+     </>
     )
 }
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import DisplayGreeting from "./DisplayGreeting";
 import AnimalProfile from "./AnimalProfile";
-import ButtonsSection from "./ButtonsSection";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
 <header><img src="assets/zoo-logo.png" className='logo' alt="" /></header>
 <DisplayGreeting />
 <div className="animalsList"></div>
-<ButtonsSection/>
 <AnimalProfile/>
 <footer>
   <p>Made for ReDI School</p>
